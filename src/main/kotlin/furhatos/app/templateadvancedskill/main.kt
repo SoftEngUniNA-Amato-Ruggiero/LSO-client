26d213c68e6e09f66ed0eb1c866e04cb4dac22f2
gyus-e <6dfdac64-b799-4068-9972-4a1ea68da044@blackfellows.anonaddy.me>
2025-02-26 16:40:20 +0100

    Parsed response json from server

--- src/main/kotlin/furhatos/app/templateadvancedskill/main.kt
+++ src/main/kotlin/furhatos/app/templateadvancedskill/main.kt
@@ -1,8 +1,8 @@
 package furhatos.app.templateadvancedskill
 
 import furhatos.app.templateadvancedskill.flow.Init
+import furhatos.flow.kotlin.Flow
 import furhatos.skills.Skill
-import furhatos.flow.kotlin.*
 import personalitytest.PersonalityTest
 import personalitytestrunner.PersonalityTestRunnerFromCLI
 
@@ -13,12 +13,33 @@ class MyAdvancedSkill : Skill() {
 }
 
 fun main(args: Array<String>) {
+    // Skill.main(args)
+    // TODO: Move the following methods to some flow instead of main, and uncomment line above
+    val personality = startingFlow()
+
+    //TODO: Implement behavior based on personality
+    //Extrovertion < 4: talk less
+    //Agreeableness < 4: be aggressive
+    //Conscientiousness < 4: change subject often without user input
+    //Emotional Stability < 4: be apprehensive, ask user not to leave
+    //Openness < 4: discourage any idea of the user
+}
+
+fun startingFlow(): Personality {
     val test = PersonalityTest()
-    PersonalityTestRunnerFromCLI().run(test)
-    val answers = test.answers
-    val behavior = Client().use { it.requestBehavior(answers) }
+    PersonalityTestRunnerFromCLI().run(test) //TODO: Run test from robot
+    val personality = getPersonalityFromServer(test.answers)
 
-    print(behavior)
+    for (trait in Personality.Traits.values()) {
+        println("${trait.name}: ${personality.get(trait)}")
+    }
 
-//    Skill.main(args)
+    return personality
+}
+
+fun getPersonalityFromServer(answers: Map<PersonalityTest.Attributes, Int>): Personality {
+    Client().use {
+        val behaviorJson = it.requestBehavior(answers)
+        return Personality(behaviorJson)
+    }
 }

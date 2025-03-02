898b9e5f4279332710fd6f8b82fdd137f823d9f7
gyus-e <6dfdac64-b799-4068-9972-4a1ea68da044@blackfellows.anonaddy.me>
2025-03-02 13:07:58 +0100

    removed dotenv dependency

--- src/main/kotlin/furhatos/app/clientlso/openaiUtils.kt
+++ src/main/kotlin/furhatos/app/clientlso/openaiUtils.kt
@@ -1,15 +1,9 @@
 package furhatos.app.clientlso
 
-import io.github.cdimascio.dotenv.Dotenv
-
 fun getApiKey(): String {
     try {
-        return Dotenv.load()["OPENAI_API_KEY"]
+        return System.getenv("OPENAI_API_KEY")
     } catch (e: Exception) {
-        try {
-            return System.getenv("OPENAI_API_KEY")
-        } catch (e: Exception) {
-            throw RuntimeException("API key not found")
-        }
+        throw RuntimeException("API key not found")
     }
 }
